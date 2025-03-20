ff0e9501a4f1892a9d226282a1a3cef2a0b6c5df
zhengzijian-ak <zhengzijian.ak@outlook.com>
2025-03-20 14:48:46 +0800

    commit the framework

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
-// import "@/styles/globals.css";
+import Script from "next/script";
+import { ThemeProvider } from "next-themes";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import { Navbar, Footer } from "../components";
+
+import "../styles/globals.css";
+
+
+const App = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar/>
+      <div className="pt-65">
+        <Component {...pageProps} />
+      </div>
+      <Footer/>
+    </div>
+
+    <Script src="https://kit.fontawesome.com/589bbe752c.js" crossorigin="anonymous" />
+  </ThemeProvider>
+  
+);
+
+export default App;
